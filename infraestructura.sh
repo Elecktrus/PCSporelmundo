2c29ed2681c77724972a5c7190153ec38da11573
Santiago <ssg900@hotmail.com>
2020-06-01 20:01:19 +0200

    Crear recursos

--- infraestructura.sh
+++ infraestructura.sh
@@ -1,3 +1,3 @@
 az group create --location westeurope -n RG1
-az storage account create -g RG1 -location westeurope -n storage1 --sku Standard_LRS --kind StorageV2
-az functionapp create --consumption-plan-location westeurope --name function1 --os-type Windows --resource-group RG1 --runtime dotnet --storage-account storage1
+az storage account create -g RG1 --location westeurope -n ssgstorage1 --sku Standard_LRS --kind StorageV2
+az functionapp create --consumption-plan-location westeurope --name ssgfunction1 --os-type Windows --resource-group RG1 --runtime dotnet --storage-account ssgstorage1
